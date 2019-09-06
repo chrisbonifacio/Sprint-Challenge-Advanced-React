5f43422c9d97309b188d546a8e2ed9763f4a769e
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-06 13:45:17 -0400

    delete package-lock.json

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { render } from "@testing-library/react";
+import PlayerList from "./components/PlayerList";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("App renders without crashing", () => {
+  render(<App />);
 });
+
+test("PlayerList renders without crashing", () => {
+  render(<PlayerList />);
+});
+
+test("PlayerCards are displaying player names", async () => {
+  const { findByTestId } = await render(<App />);
+  findByTestId("player-name");
+});
+
+test("dark mode button works correctly");
