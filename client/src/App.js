a039c0d98c2dfd9f665173f587ea8c2795c5b359
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-06 13:50:02 -0400

    add Heading

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import DarkModeButton from "./components/DarkModeButton";
 const App = () => {
   return (
     <div className="App">
+      <h1>Women's World Cup</h1>
+      <p>Ranked by search interest from Google Trends</p>
       <DarkModeButton />
       <PlayerList />
     </div>
